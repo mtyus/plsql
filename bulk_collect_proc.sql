9a9964d93efe0ec8644bb713908624b034827699
mtyus <michaeltyus3000@gmail.com>
2022-05-23 09:36:10 -0500

    Performs bulk collect with the limit clause.
    Performs bulk collect with the limit clause.

--- bulk_collect_proc.sql
+++ bulk_collect_proc.sql
@@ -3,22 +3,22 @@
 
 -- Table customer_transactions.
 CREATE TABLE customer_transactions 
-(customerid    VARCHAR2(10),
+(customer_id   NUMBER(10) PRIMARY KEY,
  trans_dte     DATE,
  trans_amt     NUMBER(9,2),
  processed_flg VARCHAR2(1));
 
 -- Table processed_transactions.
 CREATE TABLE processed_transactions 
-(customerid VARCHAR2(10),
- trans_dte  DATE,
- trans_amt  NUMBER(9,2),
- audit_flg  VARCHAR2(1));
+(customer_id   NUMBER(10) PRIMARY KEY,
+ trans_dte     DATE,
+ trans_amt     NUMBER(9,2),
+ audit_flg     VARCHAR2(1));
 
 CREATE OR REPLACE PROCEDURE bulk_collect_proc IS
 
  CURSOR c_trans(p_audit_amt NUMBER) IS 
- SELECT customerid,
+ SELECT customer_id,
         trans_dte, 
         trans_amt,
         CASE
@@ -45,7 +45,7 @@ BEGIN
 
   -- Insert customer_transaction records to processed_transactions table.
   FORALL cntr IN 1..v_trans_table.COUNT
-   INSERT INTO processed_transactions VALUES (v_trans_table(cntr).customerid,
+   INSERT INTO processed_transactions VALUES (v_trans_table(cntr).customer_id,
                                               v_trans_table(cntr).trans_dte,
                                               v_trans_table(cntr).trans_amt,
                                               v_trans_table(cntr).audit_flg);
@@ -53,7 +53,7 @@ BEGIN
   -- Update processed_flg for processed customer_transaction records.
   FORALL cntr IN 1..v_trans_table.COUNT
    UPDATE customer_transactions SET processed_flg = 'Y'
-   WHERE customerid = v_trans_table(cntr).customerid;
+   WHERE customer_id = v_trans_table(cntr).customer_id;
  
   COMMIT;
 
@@ -68,7 +68,7 @@ END;
 
 -- Insert data into customer_transactions table.
 INSERT INTO customer_transactions
-SELECT TRUNC(DBMS_RANDOM.value(1,9999999999)) AS customerid,
+SELECT TRUNC(DBMS_RANDOM.value(1,9999999999)) AS customer_id,
        TRUNC(SYSDATE + DBMS_RANDOM.value(0,366)) AS trans_dte,
        ROUND(DBMS_RANDOM.value(1,2000000),2) AS trans_amt, 
        'N' AS processed_flg
@@ -95,14 +95,14 @@ Statement processed.
 
 Statement processed.
 
-CUSTOMERID  TRANS_DTE   TRANS_AMT   AUDIT_FLG
-4607970315  12-MAY-23   151736.67   N
-2606726818  29-JUN-22   183238.68   N
-6937193069  14-JAN-23   38776.68    N
-2450782710  29-JAN-23   767952.59   N
-2389050843  17-MAR-23   1891117.3   Y
-2921381471  17-OCT-22   1628143.7   Y
-7096571348  18-FEB-23   1829276.66  Y
-966003456   17-JAN-23   1573914.97  Y
-6223451897  07-MAR-23   1283779.79  Y
-8730322564  30-DEC-22   1588102.53  Y
+CUSTOMER_ID   TRANS_DTE     TRANS_AMT     AUDIT_FLG
+9543212450    29-JUN-22     1715338.14    Y
+1476619708    03-FEB-23     1164284.29    Y
+9679171690    16-JUN-22     1812427.04    Y
+5302052966    02-OCT-22     111100.51     N
+2066970001    16-DEC-22     1044574.27    Y
+1213763411    11-DEC-22     1011398.73    Y
+3117473165    13-MAR-23     541041.02     N
+9089664233    26-JUN-22     1673444.65    Y
+4997291166    16-JUN-22     580365.9      N
+3727710996    11-MAR-23     297007.78     N
